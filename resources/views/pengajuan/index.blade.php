dc6ec7734c25370a5d6676cb571aafb881585592
Ziba Indonesia <ziba.indo@gmail.com>
2023-10-03 07:03:09 +0700

    Header

--- resources/views/pengajuan/index.blade.php
+++ resources/views/pengajuan/index.blade.php
@@ -1,49 +1,53 @@
 @extends('layouts.pengajuan')
 
 @section('content')
-    
-    {{-- START : MAIN --}}
-    <main id="main-dashboard">
 
-        {{-- START : MENU --}}
-        <section id="menu-wrapper">
-           
-            <div class="row justify-content-between">
-                <div class="col-12">
-                    <a href="{{ route('hutangoperasional.create') }}" class="menu-item">
+{{-- START : MAIN --}}
+<main id="main-dashboard">
 
-                        <p class="menu-item-title">Pengajuan Hutang Operasional</p>
-                    </a>
-                </div>
-                <div class="col-12">
-                    <a href="{{ route('operasionalkantor.create') }}" class="menu-item">
-
-                        <p class="menu-item-title">Pengajuan Operasional Kantor</p>
-                    </a>
-                </div>
-                <div class="col-12">
-                    <a href="{{ route('reimbursement.create') }}" class="menu-item">
-
-                        <p class="menu-item-title">Pengajuan Reimbursement</p>
-                    </a>
-                </div>
-                <div class="col-12">
-                    <a href="{{ route('pelatihankaryawan.create') }}" class="menu-item">
-                        <p class="menu-item-title">Pengajuan Pelatihan Karyawan</p>
-                    </a>
-                </div>
-                <div class="col-12">
-                    <a href="{{ route('csr.create') }}" class="menu-item">
-
-                        <p class="menu-item-title">Pengajuan CSR</p>
-                    </a>
-                </div>
-                
+    {{-- START : MENU --}}
+    <section id="menu-wrapper">
+        <div class="col-sm-12 col-lg-3">
+            <div style="width: 100%; height: 100%; background: #FF0000; border-top-right-radius: 10px" class="p-2 mb-3">
+                <div style="width: 100%; height: 100%; color: white; font-size: 28px; font-family: Poppins; font-weight: 700; word-wrap: break-word">Pengajuan</div>
             </div>
-        </section>
-        {{-- END : MENU --}}
-    </main>
-    {{-- END : MAIN --}}
+        </div>
+        <div class="row justify-content-between">
+            <div class="col-12">
+                <a href="{{ route('hutangoperasional.create') }}" class="menu-item">
 
-    
+                    <p class="menu-item-title">Pengajuan Hutang Operasional</p>
+                </a>
+            </div>
+            <div class="col-12">
+                <a href="{{ route('operasionalkantor.create') }}" class="menu-item">
+
+                    <p class="menu-item-title">Pengajuan Operasional Kantor</p>
+                </a>
+            </div>
+            <div class="col-12">
+                <a href="{{ route('reimbursement.create') }}" class="menu-item">
+
+                    <p class="menu-item-title">Pengajuan Reimbursement</p>
+                </a>
+            </div>
+            <div class="col-12">
+                <a href="{{ route('pelatihankaryawan.create') }}" class="menu-item">
+                    <p class="menu-item-title">Pengajuan Pelatihan Karyawan</p>
+                </a>
+            </div>
+            <div class="col-12">
+                <a href="{{ route('csr.create') }}" class="menu-item">
+
+                    <p class="menu-item-title">Pengajuan CSR</p>
+                </a>
+            </div>
+
+        </div>
+    </section>
+    {{-- END : MENU --}}
+</main>
+{{-- END : MAIN --}}
+
+
 @endsection
--- resources/views/pengajuan/operasionalkantor/create.blade.php
+++ resources/views/pengajuan/operasionalkantor/create.blade.php
@@ -7,7 +7,11 @@
 
     {{-- START : MENU --}}
     <section id="menu-wrapper">
-
+        <div class="col-sm-12 col-lg-3">
+            <div style="width: 100%; height: 100%; background: #FF0000; border-top-right-radius: 10px" class="p-2 mb-3">
+                <div style="width: 100%; height: 100%; color: white; font-size: 28px; font-family: Poppins; font-weight: 700; word-wrap: break-word">Form Pengajuan</div>
+            </div>
+        </div>
         <form>
 
             <div class="form-group">
