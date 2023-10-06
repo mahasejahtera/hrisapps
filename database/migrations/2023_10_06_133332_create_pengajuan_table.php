c82f325dc269d7e26ea264e51f2957879a71bc77
Ziba Indonesia <ziba.indo@gmail.com>
2023-10-06 13:36:15 +0700

    tabel pengajuan

---
+++ database/migrations/2023_10_06_133332_create_pengajuan_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pengajuan', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode', 5);
+            $table->string('nama', 128);
+            $table->timestamps();
+            $table->primary('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pengajuan');
+    }
+};
