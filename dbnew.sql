ab9828afafe8ae4fc1d6cc9abfd629499e898c1e
ardi firmansyah <ardifirmansy4h@gmail.com>
2023-10-28 14:25:53 +0700

    db new

--- dbnew.sql
+++ dbnew.sql
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS `karyawans` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Dumping data for table rencanakerja.karyawans: ~10 rows (approximately)
+-- Dumping data for table rencanakerja.karyawans: ~8 rows (approximately)
 DELETE FROM `karyawans`;
 INSERT INTO `karyawans` (`id`, `nama`, `email`, `kode_dept`, `role_id`, `created_at`, `updated_at`) VALUES
 	(1, 'asdas', 'asdsad', 'TK', '1', NULL, NULL),
@@ -46,14 +46,15 @@ CREATE TABLE IF NOT EXISTS `migrations` (
   `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Dumping data for table rencanakerja.migrations: ~3 rows (approximately)
+-- Dumping data for table rencanakerja.migrations: ~2 rows (approximately)
 DELETE FROM `migrations`;
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 	(5, '2019_12_14_000001_create_personal_access_tokens_table', 1),
 	(6, '2023_10_04_072959_create_rencanakerjaas_table', 2),
-	(7, '2023_10_23_023541_create_permintaan_table', 3);
+	(7, '2023_10_23_023541_create_permintaan_table', 3),
+	(8, '2023_10_28_024418_create_tugas_table', 4);
 
 -- Dumping structure for table rencanakerja.permintaan
 CREATE TABLE IF NOT EXISTS `permintaan` (
@@ -84,26 +85,18 @@ CREATE TABLE IF NOT EXISTS `permintaan` (
   KEY `permintaan_id_karyawan_pengirim_foreign` (`id_karyawan_pengirim`),
   CONSTRAINT `permintaan_id_karyawan_penerima_foreign` FOREIGN KEY (`id_karyawan_penerima`) REFERENCES `karyawans` (`id`),
   CONSTRAINT `permintaan_id_karyawan_pengirim_foreign` FOREIGN KEY (`id_karyawan_pengirim`) REFERENCES `karyawans` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=80 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Dumping data for table rencanakerja.permintaan: ~15 rows (approximately)
+-- Dumping data for table rencanakerja.permintaan: ~7 rows (approximately)
 DELETE FROM `permintaan`;
 INSERT INTO `permintaan` (`id`, `id_karyawan_penerima`, `id_karyawan_pengirim`, `perihal`, `lokasi`, `waktu`, `jatuh_tempo`, `keterangan_pengirim`, `lampiran_pengirim`, `prioritas`, `lampiran_penerima`, `keterangan_penerima`, `tolak`, `keterangan_tolak`, `status`, `manajer_approval`, `pm_approval`, `hrd_approval`, `direktur_approval`, `komisaris_approval`, `created_at`, `updated_at`) VALUES
-	(1, 1, 6, 'asdsa', 'asdasd', '2023-10-19', '2023-10-12', 'asdsa', '231023041711Final Prakerja.pdf', 'Normal', NULL, NULL, 0, 'aaaaaa', 3, 1, 1, 1, 1, 1, '2023-10-22 21:17:11', '2023-10-22 23:44:04'),
-	(2, 4, 1, 'asdsad', 'Medan', '2023-10-17', '2023-10-18', 'asdasd', '231024075310Praktik-mandiri-5-Ardi.pdf', 'Penting', 'C:\\Users\\ardif\\AppData\\Local\\Temp\\phpF6FC.tmp', 'aaaaaa', 0, 'aasdasd', 3, 1, 1, 1, 1, 1, '2023-10-24 00:53:10', '2023-10-24 01:02:42'),
-	(3, 4, 1, 'Minta File', 'Medan', '2023-10-04', '2023-10-11', 'asdas', '231024081438Praktik-mandiri-5-Ardi.pdf', 'Penting', '231024081748Praktik Kerja Mandiri 6_ Ardi Firmansyah.pdf', 'asdasdsa', 0, NULL, 4, 1, 1, 1, 1, 1, '2023-10-24 01:14:38', '2023-10-24 01:17:48'),
-	(4, 5, 4, 'Meminta Contoj', 'Medan', '2023-10-19', '2023-10-16', 'asadas', '231025020721Link github.pdf', 'Normal', NULL, NULL, 0, NULL, 1, 0, 0, 0, 0, 0, '2023-10-24 19:07:21', '2023-10-24 19:07:21'),
-	(5, 2, 1, 'Contoh ke Manajer', 'Medan Sekayang', '2023-10-20', '2023-10-17', 'Laasdasdas', '231025021826Final Prakerja.pdf', 'Mendesak', '231025023307Final Prakerja.pdf', 'aasdsad', 0, 'Contoh TOLAK', 4, 0, 1, 1, 1, 1, '2023-10-24 19:18:26', '2023-10-24 19:33:07'),
-	(6, 5, 2, 'Contoh manajer pribadi', 'Medan', '2023-10-10', '2023-10-11', 'asdasd', '231025025450Curiculum Vitae Ardi Firmansyah__.pdf', 'Mendesak', NULL, NULL, 0, 'Tidak bagus', 3, 1, 1, 1, 1, 1, '2023-10-24 19:54:50', '2023-10-24 19:59:03'),
-	(7, 5, 2, 'Contoh Diterima', 'asdas', '2023-10-03', '2023-10-04', 'asdas', '231025030402e-certificate.pdf', 'Mendesak', '231025030421Praktik-mandiri-5-Ardi.pdf', 'Bagus', 0, NULL, 4, 1, 1, 1, 1, 1, '2023-10-24 20:04:02', '2023-10-24 20:04:21'),
-	(8, 3, 2, 'Contoh ke GM', 'Medan Sekayang', '2023-10-12', '2023-10-12', 'AAAADSAAAA', '231025033141Praktik Mandiri 3_Ardi Firmansyah.pdf', 'Normal', NULL, NULL, 0, 'Tidak baguss', 3, 1, 1, 1, 1, 1, '2023-10-24 20:31:41', '2023-10-24 20:35:10'),
-	(9, 3, 2, 'Contoh ke GM Diterima', 'Medan Sekayang', '2023-10-18', '2023-10-12', 'asdasdas', '231025033627Praktik mandiri 5 Ardi.pdf', 'Mendesak', '231025033722e-certificate.pdf', 'Bagussa', 0, NULL, 4, 1, 1, 1, 1, 1, '2023-10-24 20:36:27', '2023-10-24 20:37:22'),
-	(10, 10, 3, 'Contoh Pribadi GM', 'Medan', '2023-10-19', '2023-10-24', 'ASDAS', '231025034831Curiculum Vitae Ardi Firmansyah__.pdf', 'Mendesak', '231025042531Praktik mandiri 5 Ardi.pdf', 'asdddasdsaasasass', 0, 'sadas', 4, 1, 1, 1, 1, 1, '2023-10-24 20:48:31', '2023-10-24 21:25:31'),
-	(11, 5, 10, 'Contoh HRD Pribadi', 'Medan Sekayang', '2023-10-13', '2023-10-27', 'aasdaddasddda', '231025043444Curiculum Vitae Ardi Firmansyah (1).pdf', 'Normal', NULL, NULL, 0, NULL, 1, 1, 1, 1, 1, 1, '2023-10-24 21:34:44', '2023-10-24 21:34:44'),
-	(12, 7, 2, 'Contoh KE dIREKTUR', 'Medan Sekayang', '2023-10-26', '2023-10-30', 'ASDASD', '231025064502Curiculum Vitae Ardi Firmansyah__.pdf', 'Normal', '231025064946Link github.pdf', 'Leidwwasd', 0, 'Tidak bagusss', 4, 0, 0, 0, 0, 0, '2023-10-24 23:45:02', '2023-10-24 23:49:46'),
-	(13, 5, 7, 'Contoh Direktur Pribadi', 'Medan', '2023-10-13', '2023-10-12', 'asdaaasddasd', '231025065547Curiculum Vitae Ardi Firmansyah__.pdf', 'Penting', '231025065810Praktik-mandiri-5-Ardi.pdf', 'FIleya', 0, NULL, 4, 1, 1, 1, 1, 1, '2023-10-24 23:55:47', '2023-10-24 23:58:10'),
-	(14, 8, 5, 'Contoh ke Komisaris', 'Medan Sekayang', '2023-10-19', '2023-10-12', 'asdasddasssssssd', '231025071056Final Prakerja.pdf', 'Mendesak', NULL, NULL, 0, NULL, 1, 1, 1, 1, 1, 1, '2023-10-25 00:10:56', '2023-10-25 00:10:56'),
-	(15, 7, 8, 'Minta HO', 'Medan', '2023-10-12', '2023-10-18', 'asdasd', '231025080919Praktik-mandiri-5-Ardi.pdf', 'Mendesak', NULL, NULL, 0, 'asasdasd', 3, 1, 1, 1, 1, 1, '2023-10-25 01:09:19', '2023-10-25 01:09:45');
+	(73, 7, 10, 'ke manajer pm dan direktur', 'asdsad', '2023-10-19', '2023-10-17', 'asdasd', '231027090459Final Prakerja.pdf', 'Mendesak', NULL, NULL, 0, NULL, 1, 1, 1, 1, 1, 1, '2023-10-27 02:04:59', '2023-10-27 02:04:59'),
+	(74, 2, 10, 'ke manajer biasa', 'asdas', '2023-10-17', '2023-10-12', 'asdsad', '231027092127Link github.pdf', 'Mendesak', NULL, NULL, 0, NULL, 1, 1, 1, 1, 1, 1, '2023-10-27 02:21:27', '2023-10-27 02:21:27'),
+	(75, 3, 10, 'asdsadsad', 'asdsad', '2023-10-10', '2023-10-03', 'asdsa', '231027092218Curiculum Vitae Ardi Firmansyah__ (1).pdf', 'Mendesak', NULL, NULL, 0, NULL, 1, 1, 1, 1, 1, 1, '2023-10-27 02:22:18', '2023-10-27 02:22:18'),
+	(76, 8, 10, 'komisa', 'asdsad', '2023-10-25', '2023-10-09', 'asdsa', '231027092335Curiculum Vitae Ardi Firmansyah (1).pdf', 'Mendesak', NULL, NULL, 0, NULL, 1, 1, 1, 1, 1, 1, '2023-10-27 02:23:35', '2023-10-27 02:23:35'),
+	(77, 8, 7, 'ke komsiarisa', 'Medan', '2023-10-03', '2023-10-06', 'asdsad', '231027092653Curiculum Vitae Ardi Firmansyah (1).pdf', 'Mendesak', NULL, NULL, 0, NULL, 1, 1, 1, 1, 1, 1, '2023-10-27 02:26:53', '2023-10-27 02:26:53'),
+	(78, 5, 7, 'Meminta ATK', 'Medan', '2023-10-07', '2023-10-11', 'asasd', '231027092745Praktik Mandiri 4_Ardi Firmansyah.pdf', 'Mendesak', NULL, NULL, 0, NULL, 1, 1, 1, 1, 1, 1, '2023-10-27 02:27:45', '2023-10-27 02:27:45'),
+	(79, 5, 8, 'asdasd', 'asdasd', '2023-10-19', '2023-10-26', 'asdasd', '231027093146Praktik Kerja Mandiri 6_ Ardi Firmansyah.pdf', 'Mendesak', NULL, NULL, 0, NULL, 1, 1, 1, 1, 1, 1, '2023-10-27 02:31:46', '2023-10-27 02:31:46');
 
 -- Dumping structure for table rencanakerja.personal_access_tokens
 CREATE TABLE IF NOT EXISTS `personal_access_tokens` (
@@ -150,10 +143,50 @@ CREATE TABLE IF NOT EXISTS `rencanakerjas` (
   PRIMARY KEY (`id`),
   KEY `rencanakerjas_id_karyawan_foreign` (`id_karyawan`),
   CONSTRAINT `rencanakerjas_id_karyawan_foreign` FOREIGN KEY (`id_karyawan`) REFERENCES `karyawans` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Dumping data for table rencanakerja.rencanakerjas: ~0 rows (approximately)
 DELETE FROM `rencanakerjas`;
+INSERT INTO `rencanakerjas` (`id`, `id_karyawan`, `perihal`, `lokasi`, `waktu`, `target_penyelesaian`, `keterangan`, `lampiran`, `prioritas`, `revisi`, `lampiran_revisi`, `ket_revisi`, `status`, `manajer_approval`, `pm_approval`, `hrd_approval`, `direktur_approval`, `komisaris_approval`, `created_at`, `updated_at`) VALUES
+	(1, 1, 'Meminta ATK', 'Medan', '2023-10-04', '2023-10-05', 'asdasd', '231026065602Praktik mandiri 5 Ardi.pdf', 'Normal', 0, NULL, NULL, 0, 0, 0, 0, 0, 1, '2023-10-25 23:56:02', '2023-10-25 23:56:02');
+
+-- Dumping structure for table rencanakerja.tugas
+CREATE TABLE IF NOT EXISTS `tugas` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `id_karyawan_penerima` bigint unsigned NOT NULL,
+  `id_karyawan_pengirim` bigint unsigned NOT NULL,
+  `perihal` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lokasi` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `waktu` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jatuh_tempo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `keterangan_pengirim` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lampiran_pengirim` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `prioritas` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `progress1` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `progress2` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `progress3` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `progress4` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `progress5` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `keterangan_progress` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` int NOT NULL DEFAULT '0',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `tugas_id_karyawan_penerima_foreign` (`id_karyawan_penerima`),
+  KEY `tugas_id_karyawan_pengirim_foreign` (`id_karyawan_pengirim`),
+  CONSTRAINT `tugas_id_karyawan_penerima_foreign` FOREIGN KEY (`id_karyawan_penerima`) REFERENCES `karyawans` (`id`),
+  CONSTRAINT `tugas_id_karyawan_pengirim_foreign` FOREIGN KEY (`id_karyawan_pengirim`) REFERENCES `karyawans` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Dumping data for table rencanakerja.tugas: ~3 rows (approximately)
+DELETE FROM `tugas`;
+INSERT INTO `tugas` (`id`, `id_karyawan_penerima`, `id_karyawan_pengirim`, `perihal`, `lokasi`, `waktu`, `jatuh_tempo`, `keterangan_pengirim`, `lampiran_pengirim`, `prioritas`, `progress1`, `progress2`, `progress3`, `progress4`, `progress5`, `keterangan_progress`, `status`, `created_at`, `updated_at`) VALUES
+	(1, 1, 2, 'Banyakk ha;;;;asd', 'Medan', '2023-10-04', '2023-10-11', 'asdasd', '231028040058e-certificate.pdf', 'Mendesak', NULL, NULL, NULL, NULL, NULL, NULL, 1, '2023-10-27 21:00:58', '2023-10-27 21:00:58'),
+	(2, 2, 3, 'ASDASD', 'Medan', '2023-10-17', '2023-10-25', 'ASDASDSA', '231028063425Curiculum Vitae Ardi Firmansyahh.pdf', 'Mendesak', NULL, NULL, NULL, NULL, NULL, NULL, 1, '2023-10-27 23:34:25', '2023-10-27 23:34:25'),
+	(3, 2, 3, 'asd', 'Medan', '2023-10-18', '2023-10-17', 'asdsa', '231028063644Curiculum Vitae Ardi Firmansyah__.pdf', 'Normal', NULL, NULL, NULL, NULL, NULL, NULL, 1, '2023-10-27 23:36:44', '2023-10-27 23:36:44'),
+	(4, 3, 7, 'sadasd', 'asdsad', '2023-10-07', '2023-10-22', 'asdasd', '231028065656Praktik Mandiri 3_Ardi Firmansyah.pdf', 'Mendesak', NULL, NULL, NULL, NULL, NULL, NULL, 1, '2023-10-27 23:56:56', '2023-10-27 23:56:56'),
+	(5, 3, 8, 'asdasdasd', 'asdasssssssssssssssssssssssssssssssssssssss', '2023-10-19', '2023-10-17', 'asdasd', '231028071713Praktik Mandiri 4_Ardi Firmansyah.pdf', 'Mendesak', NULL, NULL, NULL, NULL, NULL, NULL, 1, '2023-10-28 00:17:13', '2023-10-28 00:17:13'),
+	(6, 3, 8, 'asdasdasd', 'asdasssssssssssssssssssssssssssssssssssssss', '2023-10-19', '2023-10-17', 'asdasd', '231028071732Praktik-mandiri-5-Ardi.pdf', 'Mendesak', NULL, NULL, NULL, NULL, NULL, NULL, 1, '2023-10-28 00:17:32', '2023-10-28 00:17:32');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
